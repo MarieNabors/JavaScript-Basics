d10d3b88d3a1edb0e87dc9a616f781811540fc9a
Marie Nabors <marie.nabors@gmail.com>
2018-10-23 11:21:29 -0400

    functions 101 updated

--- functions/functions-101.js
+++ functions/functions-101.js
@@ -1,26 +1,38 @@
-//function -input (argument), code, output
-let greetUser = function () {
-    console.log ('Welcome user!')
+//Function - input (argument), code, output (return value)
+
+let greetUser = function (){
+    console.log('Welcome user!')
 }
+
 greetUser();
 
 let square = function (num) {
-let result = num * num
-return result
+    let result = num * num
+    return result
 }
 
 let value = square(3)
 
 let otherValue = square(10)
-console.log(value)
-console.log(otherValue)
 
-let convertFahrenheitToCelsius = function (temp) {
-    let celsius = (temp -32) * 5 / 9
+console.log(value);
+console.log(otherValue);
+
+//convert fahrenheit to celcius
+//print converted values to console
+let tempConverter= function(fahrenheit){
+    let celsius = (fahrenheit - 32) * 5/9
     return celsius
 }
 
-let tempOne = convertFahrenheitToCelsius(32)
-let tempTwo = convertFahrenheitToCelsius(68)
-console.log(tempOne)
-console.log(tempTwo)
+let tempOne = tempConverter(32)
+let tempTwo = tempConverter(68)
+
+
+console.log(tempOne);
+console.log(tempTwo);
+
+
+
+
+
