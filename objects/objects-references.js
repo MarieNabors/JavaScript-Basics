01f80659773b68a0be548b8d7f38b7c452ff65de
Marie Nabors <marie.nabors@gmail.com>
2018-10-29 12:01:29 -0400

    created account functionality

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -1,13 +1,40 @@
 let myAccount = {
-    name:'Marie Nabors',
+    //3 properties in object
+    name: 'Marie Nabors',
     expenses: 0,
     income: 0
 }
 
-let addExpense = function(account, amount){
+//create function to track additional expenses on account
+
+let addExpense = function(account, amount) {
     account.expenses = account.expenses + amount
-    
 }
 
 addExpense(myAccount, 2.50)
 console.log(myAccount)
+//addIncome
+
+let addIncome = function (account, earnings){
+    account.income = account.income + earnings
+}
+
+let resetAccount = function(account){
+    account.expenses = 0
+    account.income = 0
+}
+
+let getAccountSummary = function (account){
+    let balance = account.income - account.expenses
+    return `Account for ${account.name} has $${balance} available. $${account.income} in income. $${account.expenses} in expenses.`
+}
+addIncome(myAccount, 1000)
+console.log(myAccount)
+
+addIncome(myAccount, 2000)
+addExpense(myAccount, 2.50)
+addExpense(myAccount, 160)
+
+console.log(getAccountSummary(myAccount))
+resetAccount(myAccount)
+console.log(getAccountSummary(myAccount))
