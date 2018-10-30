40f6934d26a26b65d31182935f815b1feef71441
Marie Nabors <marie.nabors@gmail.com>
2018-10-30 11:26:50 -0400

    Update objects/string-methods.js

---
+++ objects/string-methods.js
@@ -0,0 +1,34 @@
+//All about string methods
+//length is a property of strings
+let name = 'Marie Nabors'
+//length property
+console.log(name.length)
+//convert lowercase to uppercase
+console.log(name.toUpperCase())
+
+//convert to lowercae
+console.log(name.toLowerCase())
+
+//includes method
+let password = 'abc123phello098'
+console.log(password.includes('password'))
+
+//trim method allows us to remove extra whitespace from a string, returns a new string without white space
+
+console.log(name.trim())
+
+//create a function to check if password is valid, not containing the word 'password' and at least 9 characters long
+
+let isValidPassword = function(){
+    if {
+        password.length >= 9
+        return true
+    }
+    else if {
+        password.includes('password')
+        return false
+    }
+    console.log(isValidPassword(asdfe))
+    console.log(isValidPassword('abc123@*())(&^')
+    conso
+}
