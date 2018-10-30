a7bcaa2a70d5a936a532816f5cea3ccc335759b4
Marie Nabors <marie.nabors@gmail.com>
2018-10-30 15:55:03 -0400

    Update objects/var.js

---
+++ objects/var.js
@@ -0,0 +1,18 @@
+// var firstName = 'Marie'
+
+// firstName = 'Mike'
+
+// if(10 === 10) {
+//     var firstName = 'Jen'
+// }
+
+// const setName = function(){
+//     var firstName = 'Daniel'
+// }
+// setName()
+// console.log(firstName)
+
+//var is a function scoped variable
+//let is a block scoped variable
+
+
